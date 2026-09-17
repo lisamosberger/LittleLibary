a6167a7af45fe47a03a16f2aefa8b8dc01341fad
Lisa <lisamosberger096@gmail.com>
2026-09-17 16:29:54 +0200

    Created a member Class

--- src/main/java/com/example/libary/Member.java
+++ src/main/java/com/example/libary/Member.java
@@ -1,4 +1,27 @@
 package com.example.libary;
 
 public class Member {
+
+    private String username;
+    private String password;
+    private boolean admin;
+
+
+    public Member(String name, String password, boolean admin) {
+        this.username = name;
+        this.password = password;
+        this.admin = admin;
+    }
+
+    public String getName() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean getAdmin() {
+        return admin;
+    }
 }
