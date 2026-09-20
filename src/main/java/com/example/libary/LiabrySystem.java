76569f7467b769b27cbbb29ba910ef0ca2ce7090
Lisa <lisamosberger096@gmail.com>
2026-09-20 14:35:30 +0200

    Put all input and output in LibarySystem. Fixed Errors (NullPointerexception and other system errors). Changed Structure so it's easier to follow

--- src/main/java/com/example/libary/LiabrySystem.java
+++ src/main/java/com/example/libary/LiabrySystem.java
@@ -6,7 +6,8 @@ public class LiabrySystem {
 
     static void main() {
 
-        while(true) {
+
+        while (true) {
 //translate every output to swedish!
             IO.println("\tWelcome to my little liabry!" +
                     "\n=========================================" +
@@ -18,17 +19,243 @@ public class LiabrySystem {
                     "\n6. Visa alla böcker och status" +
                     "\ne. Avsluta");
 
-            switch (IO.readln("Choose an option: ")){
-                case "1"-> libary.addBook();
-                case "2"-> libary.addMember();
-                case "3"-> libary.addLoan();
-                case "4"-> libary.returnLoan();
-                case "5"-> libary.findBook();
-                case "6"-> libary.showAllBooks();
-                case "e"-> System.exit(0);
+            switch (IO.readln("Choose an option: ")) {
+                case "1" -> addBook();
+                case "2" -> addMember();
+                case "3" -> borrowBook();
+                case "4" -> returnBook();
+                case "5" -> findBook();
+                case "6" -> showAllBooks();
+                case "e" -> System.exit(0);
                 default -> IO.println("Invalid input");
             }
 
+            IO.readln("Press Enter to continue...");
+
+        }
+
+    }
+
+    static void findBook() {
+        IO.println("Find Book by: " +
+                "\n1. Title" +
+                "\n2. Author" +
+                "\n3. ISBN");
+
+        String choice = IO.readln("Choose an option: ");
+
+        switch (choice) {
+            case "1" -> findBookByTitle();
+            case "2" -> findBookByAuthor();
+            case "3" -> findBookByIsbn();
+            default -> IO.println("Invalid input");
+
         }
     }
+
+    static void printBook(Book book) {
+
+        IO.println("Title: " + book.title() +
+                "\nAuthor: " + book.author() +
+                "\nISBN: " + book.isbn());
+
+    }
+
+    static int checkIfNumber(String input) {
+
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return -1;
+            }
+        }
+        return Integer.parseInt(input);
+    }
+
+
+    static void findBookByIsbn() {
+        String input = IO.readln("Enter ISBN: ");
+        int isbn = checkIfNumber(input);
+
+        Book book = libary.findBookByIsbn(isbn);
+
+        if (book == null) {
+            IO.println("Invalid input");
+            return;
+        }
+
+        printBook(book);
+
+    }
+
+    static void findBookByTitle() {
+        String title = IO.readln("Enter title: ");
+
+        Book book = libary.findBookByTitle(title);
+
+        if (book == null) {
+            IO.println("Invalid input");
+            return;
+        }
+
+        printBook(book);
+
+    }
+
+    static void findBookByAuthor() {
+        String author = IO.readln("Enter author: ");
+
+        Book book = libary.findBookByAuthor(author);
+
+        if (book == null) {
+            IO.println("Invalid input");
+            return;
+        }
+
+        printBook(book);
+
+    }
+
+    static void addBook() {
+        String input = IO.readln("Enter ISBN: ");
+        int isbn = checkIfNumber(input);
+        if (isbn == -1) {
+            IO.println("Invalid input");
+            return;
+        }
+
+        String title = IO.readln("Enter Title: ");
+        String author = IO.readln("Enter Author: ");
+
+
+        Book book = new Book(isbn, title, author);
+
+        if (libary.addBook(book)) {
+            IO.println("Book added successfully!");
+        } else {
+            IO.println("Book could not be added!");
+        }
+    }
+
+    static void showAllBooks() {
+
+        for (int i = 0; i < libary.getBookCounter(); i++) {
+
+            Book book = libary.getBook(i);
+            printBook(book);
+
+            Loan loan = libary.findLoan(book);
+
+            if (loan != null) {
+                IO.println("Borrowed by: " + libary.findLoan(book).getMember().getUsername());
+            } else {
+                IO.println("The Book is available!");
+            }
+        }
+
+    }
+
+    static void borrowBook() {
+        showAllBooks();
+
+        String title = IO.readln("Which book do you want to borrow: "+
+                "Name it by title: ");
+
+        Book book = libary.findBookByTitle(title);
+
+        if (book == null){
+            IO.println("Book not found!");
+            return;
+        }
+
+        String username = IO.readln("Enter Username: ");
+        Member member = libary.findMember(username);
+
+        if (member == null){
+            IO.println("Member not found!");
+            return;
+        }
+
+        if (libary.findLoan(book) != null){
+            IO.println("This Book is already borrowed!");
+            return;
+        }
+        else if (libary.getLoanCounter() >= libary.getLoanLenght()){
+            IO.println("There is no more space in the libary for new loan.");
+            return;
+        }
+
+        Loan loan = new Loan(book, member);
+        libary.borrowBook(loan);
+        IO.println("You borrowed the book successfully!");
+    }
+
+    static void returnBook() {
+
+        String username = IO.readln("Enter your username: ");
+
+        if (libary.findMember(username) == null) {
+            IO.println("This user does not exist!");
+            return;
+        }
+
+
+        IO.println("Your Loan List:" +
+                "\n============================");
+        Loan loanByMember = libary.findLoanByMember(username);
+
+        if (loanByMember == null) {
+            IO.println("This user has no Borrowed Books!");
+            return;
+        }
+
+        for (int i = 0; i < libary.getLoanCounter(); i++) {
+            IO.println("Loan: " + loanByMember.getBook().isbn() +
+                    "\nTitle: " + loanByMember.getBook().title() +
+                    "\nAuthor: " + loanByMember.getBook().author());
+        }
+        String title = IO.readln("Which book do you want to return? Name title: ");
+
+        if (libary.findBookByTitle(title) == null) {
+            IO.println("The book does not exist!");
+        }
+
+        libary.returnBook(username, title);
+        IO.println("Book returned successfully!");
+
+
+    }
+
+    static void addMember(){
+
+        String username = IO.readln("Enter username: ");
+        String password = IO.readln("Enter password: ");
+        String admin = IO.readln("Enter y for admin n for user: ");
+        Boolean a;
+        if (admin.equals("y") ) {
+            a = true;
+        }
+        else if (admin.equals("n") ) {
+            a = false;
+        }
+        else {
+            IO.println("Invalid input!");
+            return;
+        }
+
+        Member member = new Member(username, password, a);
+
+        libary.addMember(member);
+    }
+
+    //put all input and output here, seperate libary from input and output/better structure
+
+    /*
+    sort it after:
+    - Menu (Main Menu)
+    - Book (missing: sortAllBooks)
+    - Member (addMember, showAllMember, sortAllMember)
+    - Loan (addLoan, reaturnLoan)
+     */
+
+
 }
--- src/main/java/com/example/libary/Libary.java
+++ src/main/java/com/example/libary/Libary.java
@@ -1,8 +1,10 @@
 package com.example.libary;
 
+import static com.example.libary.LiabrySystem.showAllBooks;
+
 public class Libary {
 
-    private Book [] books = new Book[10];
+    Book [] books = new Book[10];
     private Member [] members = new Member[10];
     private Loan [] loans = new Loan[10];
 
@@ -10,12 +12,30 @@ public class Libary {
     private int memberCounter = 0;
     private int loanCounter = 0;
 
-    //check if book already exist before adding it with isbn!
+    public int getBookCounter() {
+        return bookCounter;
+    }
+
+    public int getMemberCounter() {
+        return memberCounter;
+    }
+
+    public int getLoanLenght() {
+        return loans.length;
+    }
+
+    public int getLoanCounter() {
+        return loanCounter;
+    }
+
+    /*
+     Check if it's a number or letter (Character.isDigit)
+     Create a Method for it (checkIfNumber) static!
+     */
 
 
 
-
-    public Book findBook (int isbn) {
+    public Book findBookByIsbn(int isbn) {
 
         for (int i = 0; i < bookCounter; i++){
 
@@ -26,139 +46,139 @@ public class Libary {
         return null;
     }
 
+    public Book findBookByAuthor (String author) {
 
-    public void addBook(){
+        for  (int i = 0; i < bookCounter; i++){
 
-        int isbn = Integer.parseInt(IO.readln("Enter ISBN: "));
-        String title = IO.readln("Enter Title: ");
-        String author = IO.readln("Enter Author: ");
+            if (books[i].author().equalsIgnoreCase(author)){
+                return books[i];
+            }
+        }
+        return null;
+    }
 
-        Book book = new Book(isbn, title, author);
+    public Book findBookByTitle (String title) {
 
-        if (findBook(book.isbn()) != null){
-            IO.println("Book already exists!");
+
+        for (int i = 0; i < bookCounter; i++){
+
+            if (books[i].title().equalsIgnoreCase(title)){
+            return books[i];
+            }
+        }
+        return null;
+    }
+
+    public boolean addBook(Book book) {
+
+
+        if (findBookByIsbn(book.isbn()) != null){
+            return false;
         }
         if (bookCounter >= books.length){
-            IO.println("There is no more space in the libary for new books.");
-            return;
+            return false;
         }
         books[bookCounter] = book;
         bookCounter++;
+        return true;
     }
     
     public Member findMember(String username) {
         for (int i = 0; i < memberCounter; i++) {
-            if (members[i].getUsername().equals(username)) {
+            if (members[i].getUsername().equalsIgnoreCase(username)) {
                 return members[i];
             }
         }
         return null;
     }
     
-    public void addMember(){
+    public void addMember(Member member) {
 
-        String username = IO.readln("Enter Username: ");
-        String password = IO.readln("Enter Password: ");
-        String admin = IO.readln("Are you an admin yes or no (y/n): ");
-        boolean a;
-
-            if (admin.equals("y")){
-                a = true;
-            }
-            else if (admin.equals("n")){
-                a = false;
-            }
-            else {
-                IO.println("Invalid Input.");
-                return;
-            }
-            Member member = new Member(username, password, a);
 
         if (findMember(member.getUsername()) != null){
             IO.println("Member already exists!");
+            return;
         }
         else if (memberCounter >= members.length){
             IO.println("There is no more space in the libary for new members.");
+            return;
         }
         members[memberCounter] = member;
         memberCounter++;
     }
 
-    public Loan findLoan(Loan loan) {
+    public Loan findLoan(Book book) {
         for (int i = 0; i < loanCounter; i++){
 
-            if (loans[i].getBook().isbn() == loan.getBook().isbn()){
+            if (loans[i].getBook().isbn() == book.isbn()){
                 return loans[i];
             }
         }
         return null;
     }
 
-    public void addLoan(Loan loan){
-        if (findLoan(loan) != null){
-            IO.println("This Book is already borrowed!");
+    public Loan findLoanByMember(String member) {
+
+        for (int i = 0; i < loanCounter; i++){
+            if (member.equalsIgnoreCase(loans[i].getMember().getUsername())){
+                return loans[i];
+            }
         }
-        else if (loanCounter >= loans.length){
-            IO.println("There is no more space in the libary for new loan.");
-        }
+        return null;
+    }
+
+
+    public void borrowBook(Loan loan){
+
+
         loans[loanCounter] = loan;
         loanCounter++;
     }
 
-   public void returnLoan(Loan loan){
-        if (findLoan(loan) == null){
-            IO.println("This book is not borrowed!");
-        return;
-        }
+   public void returnBook(String username, String title){
+
         for (int i = 0; i < loanCounter; i++){
-            if (loans[i].getBook().isbn() == loan.getBook().isbn()){
+
+            if (loans[i].getMember().getUsername().equalsIgnoreCase(username) &&
+                   loans[i].getBook().title().equalsIgnoreCase(title)){
                 for (int j = i; j < bookCounter; j++){
-                loans[j] = loans[j + i];}
-            }
-            loans[loanCounter - 1] = null;
-            loanCounter--;
-            return;
-        }
-   }
-
-
-   public void showAllBooks(Book book){
-        for (int i = 0; i < bookCounter; i++) {
-
-            IO.println("Book " + (i + 1) + ": " + books[i].title() +
-                    "\nISBN: " + books[i].isbn() +
-                    "\n Author: " + books[i].author());
-
-
-            boolean borrowed = false;
-
-            for (int j = 0; j < loanCounter; j++){
-                if (loans[j].getBook().isbn() == book.isbn()){
-                    borrowed = true;
-
-                    IO.println("Status Borrowed by " +
-                            loans[j].getMember().getUsername());
-                    break;
+                loans[j] = loans[j + i];
                 }
+
+                loans[loanCounter - 1] = null;
+                loanCounter--;
             }
 
-            if (!borrowed){
-                IO.println("The Book is Avaiable.");
-            }
-
-            IO.println("========================================================\n");
         }
-
    }
 
+public Book getBook(int i){
+        return books[i];
+}
 
-    //show all books (in order)
+   public boolean isBorrowed(Book book){
 
-    //find book by isbn
+       for (int i = 0; i < loanCounter; i++){
+           if (loans[i].getBook().isbn() == books[i].isbn()){
+             return true;
+           }
+       }
+    return  false;
+   }
+
+   //fix your borrowed books print!
+
+   //check if isbn is a number
+
+    //make capitalisation not important anymore (maybe with lowerto or upperto?) - use equalsIgnoreCase
+
+    //show all members (also in order?)
 
     //Method for showMemberWithMostLoans
 
     //Change size of possible loans/books/members
+
+    //Login menu( admin/user)
 
 }
 
